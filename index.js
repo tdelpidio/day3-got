2d2ccf7d0bd695b51ff41c67618d8e255e5ea1b0
tdelpidio <tdelpidio@gmail.com>
2022-05-25 14:24:20 -0400

    fixed tie breaker issue

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let jamieLannisterAttack = 35;
 
 if (jonSnowAttack > jamieLannisterAttack) {
     console.log("Jon Snow has better attack than Jamie Lannister");
-} else {
+} else if (jonSnowAttack < jamieLannisterAttack) {
     console.log("Jamie Lanister has better attack than Jon Snow");
+} else {
+    console.log("Jon Snow and Jamie Lannister have the same attack");
 }
