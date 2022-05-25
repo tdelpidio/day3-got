7732dc6dba1dab86979c8a6933badcf96cb12c4b
tdelpidio <tdelpidio@gmail.com>
2022-05-25 14:30:08 -0400

    Jamie's first attack

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+// Jon's stats
 let jonSnowAttack = 25;
-let jamieLannisterAttack = 35;
+let jonSnowHealth = 100;
+let jonSnowDefense = 0;
+
+// Jamie's stats
+let jamieLannisterAttack = 107;
 
 if (jonSnowAttack > jamieLannisterAttack) {
     console.log("Jon Snow has better attack than Jamie Lannister");
@@ -7,4 +12,12 @@ if (jonSnowAttack > jamieLannisterAttack) {
     console.log("Jamie Lanister has better attack than Jon Snow");
 } else {
     console.log("Jon Snow and Jamie Lannister have the same attack");
+}
+
+if (jonSnowHealth <= jamieLannisterAttack) {
+    console.log("Jon Snow has been slain");
+} else {
+    // jonSnowHealth = jonSnowHealth - jamieLannisterAttack;
+    jonSnowHealth -= jamieLannisterAttack;
+    console.log(`Jon Snow's health is down to ${jonSnowHealth}`);
 }
